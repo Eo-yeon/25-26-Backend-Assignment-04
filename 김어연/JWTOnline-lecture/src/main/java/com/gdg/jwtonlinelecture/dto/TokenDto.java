0dbf25f281fe80f6801ba2ddc4511da33c092a99
kimeoyeon <djdus126@gmail.com>
2025-11-12 19:26:58 +0900

    refactor: NoArgsConstructor 및 Builder 제거

--- 김어연/JWTOnline-lecture/src/main/java/com/gdg/jwtonlinelecture/dto/TokenDto.java
+++ 김어연/JWTOnline-lecture/src/main/java/com/gdg/jwtonlinelecture/dto/TokenDto.java
@@ -1,14 +1,10 @@
 package com.gdg.jwtonlinelecture.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
 @AllArgsConstructor
 @Getter
-@Builder
 public class TokenDto {
     private String accessToken;
 }
