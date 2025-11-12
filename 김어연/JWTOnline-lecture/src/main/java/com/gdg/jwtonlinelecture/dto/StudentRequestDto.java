99665a29009e0f68999d52e63edd4a8c29dc761c
kimeoyeon <djdus126@gmail.com>
2025-11-12 11:18:42 +0900

    refactor: StudentRequestDto에서 @Setter 제거

--- 김어연/JWTOnline-lecture/src/main/java/com/gdg/jwtonlinelecture/dto/StudentRequestDto.java
+++ 김어연/JWTOnline-lecture/src/main/java/com/gdg/jwtonlinelecture/dto/StudentRequestDto.java
@@ -1,14 +1,14 @@
 package com.gdg.jwtonlinelecture.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class StudentRequestDto {
     private String name;
     private String email;
